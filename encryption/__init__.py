143467d59d2f6ec05d83d632ee095c15291e6a44
Boon Han Koh <boonhan.koh@gmail.com>
2026-06-23 16:43:29 +0100

    Extend lookup table to all 26 letters of alphabet

--- encryption/__init__.py
+++ encryption/__init__.py
@@ -1,3 +1,4 @@
+import string
 from operator import and_
 
 from otree.api import *
@@ -23,12 +24,12 @@ class Subsession(BaseSubsession):
     def setup_round(self):
         self.payment_per_correct = Currency(C.PAYMENT_PER_CORRECT)
         self.word="AB"
-        self.lookup_table = "BA"
+        self.lookup_table = string.ascii_uppercase
 
     @property
     def lookup_dict(self):
         lookup = {}
-        for letter in ["A", "B"]:
+        for letter in string.ascii_uppercase:
             lookup[letter] = self.lookup_table.index(letter)+1
         return lookup
 
